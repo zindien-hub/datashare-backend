da249ea0cd0545449b17a0f7f38ad8b58b085053
JR Jernival <267707087+zindien-hub@users.noreply.github.com>
2026-04-30 12:57:17 +0200

    chore: initialize spring boot backend

---
+++ src/test/java/datashare_backend/DatashareBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package datashare_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatashareBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
