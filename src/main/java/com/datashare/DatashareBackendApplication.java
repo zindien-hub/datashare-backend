2f48ae144972b1ca0c03e52ef7567c87c51ad576
JR Jernival <267707087+zindien-hub@users.noreply.github.com>
2026-05-03 18:23:16 +0200

    refactor: align backend base package with groupId

---
+++ src/main/java/com/datashare/DatashareBackendApplication.java
@@ -0,0 +1,12 @@
+package com.datashare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatashareBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatashareBackendApplication.class, args);
+	}
+}
